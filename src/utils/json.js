a4716278ebe57f3e0e08569ffeeb6680b2dbe62c
Siva-1702 <69497647+Siva-1702@users.noreply.github.com>
2026-03-24 00:01:57 +0530

    Implement real Google OAuth flows

---
+++ src/utils/json.js
@@ -0,0 +1,22 @@
+const readJsonBody = async (req) => {
+  const chunks = [];
+  for await (const chunk of req) {
+    chunks.push(chunk);
+  }
+  if (!chunks.length) {
+    return {};
+  }
+  return JSON.parse(Buffer.concat(chunks).toString('utf8'));
+};
+
+const sendJson = (res, statusCode, payload) => {
+  const body = JSON.stringify(payload);
+  res.writeHead(statusCode, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(body),
+    'Access-Control-Allow-Origin': '*'
+  });
+  res.end(body);
+};
+
+module.exports = { readJsonBody, sendJson };
